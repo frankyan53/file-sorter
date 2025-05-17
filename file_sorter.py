853072fc534a2ce1d0265fbb0cf2c21305089e42
Frank Yan <frankyan53@gmail.com>
2025-05-16 23:50:11 -0700

    chore: scaffold core functions

---
+++ file_sorter.py
@@ -0,0 +1,22 @@
+def create_child_folders():
+    pass
+
+
+def list_files():
+    pass
+
+
+def categorize_file():
+    pass
+
+
+def move_file():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
