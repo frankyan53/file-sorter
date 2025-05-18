df1777b381b4f5fcec0d69a5e2b5ccda55882beb
Frank Yan <frankyan53@gmail.com>
2025-05-17 18:25:44 -0700

    chore: delete get_operating_system() for simplification and GUI-compatibility

--- file_sorter.py
+++ file_sorter.py
@@ -1,40 +1,7 @@
-import textwrap
 from pathlib import Path
 
 
-def get_operating_system():
-    print("""Below is a list of operating systems.
-1. Windows
-2. macOS
-3. Linux""")
-    operating_system = input(
-        "Enter a number from 1 to 3 to choose the operating system that you are currently using: ").strip()
-    while operating_system not in ("1", "2", "3"):
-        operating_system = input(
-            "Invalid number. Please try again. Enter a number from 1 to 3: ").strip()
-    print("\nThank you for choosing your operating system.\n")
-    if operating_system == "1":
-        print(textwrap.fill(
-            "To start sorting, enter the path of the folder you want to organize.", 80))
-        print()
-        print(textwrap.fill("Open File Explorer, navigate to the folder, then click the path bar at the top. Copy the full path, and paste it when prompted. Alternatively, open Terminal and drag the folder into the window to show the full path.", 80))
-        print()
-    elif operating_system == "2":
-        print(textwrap.fill(
-            "To start sorting, enter the path of the folder you want to organize.", 80))
-        print()
-        print(textwrap.fill("Open Finder, find the folder, then right-click and choose \"Get Info.\" Copy the full path from the \"Where\" section, and paste it when prompted. Alternatively, open Terminal and drag the folder into the window to show the full path.", 80))
-        print()
-    else:
-        print(textwrap.fill(
-            "To start sorting, enter the path of the folder you want to organize.", 80))
-        print()
-        print(textwrap.fill("Open your file manager (e.g. Nautilus), find the folder, then right-click and choose \"Properties.\" Copy the full path, and paste it when prompted. Alternatively, open Terminal and drag the folder into the window to show the full path.", 80))
-        print()
-
-
 def get_parent_folder_path():
-    get_operating_system()
     for i in range(3, 0, -1):
         parent_folder_path_input = input(
             f"Enter folder path ({i} attempts remaining): ").replace("\\", "/").strip()
