dbc32807e1bfe6ce2fcd71cecd1609d04d5d8d52
Frank Yan <frankyan53@gmail.com>
2025-05-17 18:13:12 -0700

    feat: extract child_folders.json and child_folder_extensions.json as config files from logic.py and update logic.py to load it

--- logic.py
+++ logic.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 
 def create_child_folders(parent_folder_path):
-    child_folders_list = ["3D Models", "Archives", "Audio", "Code", "Documents",
-                          "Executables", "Fonts", "Images", "Other", "Spreadsheets", "Videos"]
+    with open("child_folders.json") as file:
+        child_folders_list = json.load(file)
     try:
         for folder in child_folders_list:
             child_folder_path = parent_folder_path / folder
@@ -15,19 +15,8 @@ def create_child_folders(parent_folder_path):
 
 
 def move_files(parent_folder_path):
-    child_folder_extensions_dict = {
-        "3D Models": [".obj", ".fbx", ".stl", ".3ds", ".dae", ".blend", ".gltf", ".glb", ".x3d"],
-        "Archives": [".zip", ".rar", ".tar", ".gz", ".7z", ".bz2", ".xz", ".cab", ".iso", ".lz", ".lzma", ".z", ".ace", ".arj", ".jar"],
-        "Audio": [".mp3", ".wav", ".aac", ".flac", ".ogg", ".m4a", ".wma", ".alac", ".aiff", ".amr", ".mid", ".midi", ".opus", ".ra", ".pcm"],
-        "Code": [".py", ".js", ".html", ".css", ".cpp", ".c", ".java", ".rb", ".sh", ".json", ".xml", ".ipynb", ".php", ".go", ".rs", ".swift", ".ts", ".tsx", ".jsx", ".cs", ".vb", ".kt", ".scala", ".pl", ".lua", ".sql", ".yml", ".yaml", ".bat", ".cmd", ".ini", ".cfg"],
-        "Documents": [".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx", ".txt", ".rtf", ".odt", ".odp", ".ods", ".epub", ".md", ".tex", ".log", ".csv", ".pages", ".numbers", ".key", ".xps"],
-        "Executables": [".exe", ".msi", ".dmg", ".pkg", ".bat", ".sh", ".app", ".deb", ".rpm", ".run", ".apk", ".bin", ".com", ".command", ".gadget"],
-        "Fonts": [".ttf", ".otf", ".woff", ".woff2", ".eot", ".fon"],
-        "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".svg", ".heic", ".ico", ".raw", ".cr2", ".nef", ".orf", ".sr2", ".psd", ".ai", ".eps"],
-        "Other": [],
-        "Spreadsheets": [".xls", ".xlsx", ".ods", ".csv", ".tsv"],
-        "Videos": [".mp4", ".mkv", ".mov", ".avi", ".flv", ".wmv", ".webm", ".mpeg", ".mpg", ".3gp", ".m4v", ".ts", ".mts", ".vob", ".rm", ".rmvb"]
-    }
+    with open("child_folder_extensions.json") as file:
+        child_folder_extensions_dict = json.load(file)
     for source_file_path in parent_folder_path.iterdir():
         if source_file_path.is_dir():
             continue
