ef70671f744648d944cfd9d4a0cb164de9a8ee0e
Frank Yan <frankyan53@gmail.com>
2025-05-17 22:58:49 -0700

    feat: log original and renamed file names in original_file_names.json and renamed_file_names.json

--- logic.py
+++ logic.py
@@ -25,6 +25,10 @@ def create_child_folders(parent_folder_path):
 
 
 def move_files(parent_folder_path):
+    with open("original_file_names.json") as file:
+        original_file_names = json.load(file)
+    with open("renamed_file_names.json") as file:
+        renamed_file_names = json.load(file)
     with open("child_folder_extensions.json") as file:
         child_folder_extensions_dict = json.load(file)
     report = {
@@ -34,14 +38,27 @@ def move_files(parent_folder_path):
             continue
         for child_folder, child_folder_extensions in child_folder_extensions_dict.items():
             if source_file_path.suffix in child_folder_extensions:
-                destination_file_path = (
+                original_destination_file_path = (
                     source_file_path.parent / child_folder / source_file_path.name)
-                destination_file_path = get_unique_path(destination_file_path)
-                shutil.move(source_file_path, destination_file_path)
+                renamed_destination_file_path = get_unique_path(
+                    original_destination_file_path)
+                shutil.move(source_file_path, renamed_destination_file_path)
+                if original_destination_file_path != renamed_destination_file_path:
+                    original_file_names.append(
+                        original_destination_file_path.name)
+                    renamed_file_names.append(
+                        renamed_destination_file_path.name)
                 report[child_folder] += 1
                 break
         else:
-            destination_file_path = source_file_path.parent / "Other" / source_file_path.name
-            shutil.move(source_file_path, destination_file_path)
+            original_destination_file_path = (
+                source_file_path.parent / "Other" / source_file_path.name)
+            renamed_destination_file_path = get_unique_path(
+                original_destination_file_path)
+            shutil.move(source_file_path, renamed_destination_file_path)
             report["Other"] += 1
+    with open("original_file_names.json", "w") as file:
+        json.dump(original_file_names, file, indent=4)
+    with open("renamed_file_names.json", "w") as file:
+        json.dump(renamed_file_names, file, indent=4)
     return report
