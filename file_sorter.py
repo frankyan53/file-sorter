613130f8458d4515303ca37120195d6f31741075
Frank Yan <frankyan53@gmail.com>
2025-05-17 20:57:36 -0700

    feat: rename file_sorter.py to main.py and set up main control flow

--- file_sorter.py
+++
@@ -1,8 +0,0 @@
-def main():
-    parent_folder_path = get_parent_folder_path()
-    create_child_folders(parent_folder_path)
-    move_file(parent_folder_path)
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,21 @@
+from cli import get_parent_folder_path
+from logic import create_child_folders, move_files
+
+
+def main():
+    parent_folder_path = get_parent_folder_path()
+    if create_child_folders(parent_folder_path):
+        print("Folders created successfully. Organizing files now...\n")
+    else:
+        print("Encountered error when creating folders. Exiting.")
+        return
+    report = move_files(parent_folder_path)
+    for child_folder, files_moved in report.items():
+        if files_moved > 0:
+            print(f"{"Sort Report":=^40}")
+            print(
+                f"{files_moved} file{"s" if files_moved != 1 else ""} moved into {child_folder}.")
+
+
+if __name__ == "__main__":
+    main()
