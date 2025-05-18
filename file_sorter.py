c25eece9730c5aa60944e771df00c53eddf2b47a
Frank Yan <frankyan53@gmail.com>
2025-05-17 18:32:04 -0700

    refactor: extract cli.py from file_sorter.py

--- file_sorter.py
+++ file_sorter.py
@@ -1,21 +1,3 @@
-from pathlib import Path
-
-
-def get_parent_folder_path():
-    for i in range(3, 0, -1):
-        parent_folder_path_input = input(
-            f"Enter folder path ({i} attempts remaining): ").replace("\\", "/").strip()
-        parent_folder_path = Path(parent_folder_path_input)
-        if parent_folder_path.exists() and parent_folder_path.is_dir():
-            return parent_folder_path
-        if i != 1:
-            print(f"Invalid folder path. Please try again.", end=" ")
-        else:
-            print(
-                "\nInvalid path entered 3 times. Defaulting to current working directory.", end=" ")
-            return Path.cwd()
-
-
 def main():
     parent_folder_path = get_parent_folder_path()
     create_child_folders(parent_folder_path)
