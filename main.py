1cf6f3dd5d402b00c4f05fcfa26b9c1d9b32b315
Frank Yan <frankyan53@gmail.com>
2025-05-19 02:15:17 -0700

    refactor: extract logic return values in main() and pass as arguments to output helpers

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from cli import get_parent_folder_path
+from logic import create_folders, sort_files, unsort_files, delete_empty_folders
 from output import print_create_errors, print_sort_errors, print_no_files, print_renamed_sort_files, print_unsort_errors, print_renamed_unsort_files, print_delete_errors
 
 
 def main():
     parent_folder_path = get_parent_folder_path()
-    print_sort_errors(parent_folder_path)
-    print_create_errors(parent_folder_path)
-    print_sort_errors(parent_folder_path)
-    print_no_files(parent_folder_path)
-    print_renamed_sort_files(parent_folder_path)
-    print_unsort_errors(parent_folder_path)
-    print_renamed_unsort_files(parent_folder_path)
-    print_delete_errors(parent_folder_path)
+    is_successful, create_errors = create_folders(parent_folder_path)
+    print_create_errors(is_successful, create_errors)
+    has_source_files, report, renamed_files, is_successful, sort_errors = sort_files(
+        parent_folder_path)
+    print_sort_errors(is_successful, sort_errors)
+    print_no_files(has_source_files, report)
+    print_renamed_sort_files(renamed_files)
+    renamed_files, is_successful, unsort_errors = unsort_files(
+        parent_folder_path)
+    print_unsort_errors(is_successful, unsort_errors)
+    print_renamed_unsort_files(renamed_files)
+    is_successful, delete_errors = delete_empty_folders(parent_folder_path)
+    print_delete_errors(is_successful, delete_errors)
 
 
 if __name__ == "__main__":
--- output.py
+++ output.py
@@ -1,8 +1,7 @@
 from logic import create_folders, sort_files, unsort_files, delete_empty_folders
 
 
-def print_create_errors(parent_folder_path):
-    is_successful, create_errors = create_folders(parent_folder_path)
+def print_create_errors(is_successful, create_errors):
     if not is_successful and create_errors:
         print(
             f"Failed to create any folders. Encountered {len(create_errors)} error{"s" if len(create_errors) != 1 else ""}. See \"create_errors.json\" for more details.")
@@ -13,8 +12,7 @@ def print_create_errors(parent_folder_path):
         print("Folders created successfully.")
 
 
-def print_sort_errors(parent_folder_path):
-    _, _, _, is_successful, sort_errors = sort_files(parent_folder_path)
+def print_sort_errors(is_successful, sort_errors):
     if not is_successful and sort_errors:
         print(
             f"Failed to sort any files. Encountered {len(sort_errors)} error{"s" if len(sort_errors) != 1 else ""}. See \"sort_errors.json\" for more details.")
@@ -27,10 +25,9 @@ def print_sort_errors(parent_folder_path):
         return
 
 
-def print_no_files(parent_folder_path):
-    has_source_files, report, _, _, _ = sort_files(parent_folder_path)
+def print_no_files(has_source_files, report):
     if not has_source_files:
-        print("No files available to sort")
+        print("No files available to sort.")
     else:
         print(f"{"Sort Report":=^40}")
         for child_folder, files_moved in report.items():
@@ -39,15 +36,13 @@ def print_no_files(parent_folder_path):
                     f"{files_moved} file{"s" if files_moved != 1 else ""} moved into {child_folder}.")
 
 
-def print_renamed_sort_files(parent_folder_path):
-    _, _, renamed_files, _, _ = sort_files(parent_folder_path)
+def print_renamed_sort_files(renamed_files):
     for file in renamed_files:
         print(
             f"{file["original"]} was renamed to {file["renamed"]} due to a duplicate filename.")
 
 
-def print_unsort_errors(parent_folder_path):
-    _, is_successful, unsort_errors = unsort_files(parent_folder_path)
+def print_unsort_errors(is_successful, unsort_errors):
     if not is_successful and unsort_errors:
         print(
             f"Failed to unsort any files. Encountered {len(unsort_errors)} error{"s" if len(unsort_errors) != 1 else ""}. See \"unsort_errors.json\" for more details.")
@@ -58,15 +53,13 @@ def print_unsort_errors(parent_folder_path):
         print("Files unsorted successfully.")
 
 
-def print_renamed_unsort_files(parent_folder_path):
-    renamed_files, _, _ = unsort_files(parent_folder_path)
+def print_renamed_unsort_files(renamed_files):
     for file in renamed_files:
         print(
             f"{file["original"]} was renamed to {file["renamed"]} due to a duplicate filename.")
 
 
-def print_delete_errors(parent_folder_path):
-    is_successful, delete_errors = delete_empty_folders(parent_folder_path)
+def print_delete_errors(is_successful, delete_errors):
     if not is_successful and delete_errors:
         print(
             f"Failed to delete any folders. Encountered {len(delete_errors)} error{"s" if len(delete_errors) != 1 else ""}. See \"delete_errors.json\" for more details.")
