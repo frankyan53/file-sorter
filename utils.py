ea4041b9ec2f370fa3540ca944f86faf9037722c
Frank Yan <frankyan53@gmail.com>
2025-05-18 23:19:37 -0700

    refactor: move helper functions to separate module and remove unused imports from logic

---
+++ utils.py
@@ -0,0 +1,32 @@
+import json
+
+
+def load_json(file_path):
+    with open(file_path) as file:
+        return json.load(file)
+
+
+def write_json(file_path, data):
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def get_unique_path(original_file_path):
+    counter = 1
+    renamed_file_path = original_file_path
+    while renamed_file_path.exists():
+        renamed_file_path = original_file_path.with_stem(
+            f"{original_file_path.stem}_{counter}")
+        counter += 1
+    return renamed_file_path
+
+
+def append_rename_dict(renamed_files_list, original_file_path, renamed_file_path):
+    if original_file_path != renamed_file_path:
+        renamed_files_list.append(
+            {"original": original_file_path.name, "renamed": renamed_file_path.name})
+
+
+def append_error_dict(errors_list, directory, operation, error):
+    errors_list.append({"directory": str(directory),
+                       "operation": operation, error: str(error)})
